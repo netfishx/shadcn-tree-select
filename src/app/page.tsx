7f3acbd7a3fc9b6dd1ff58db68da66dbb0a3cf7c
ethan <wp_wang@manka.co.jp>
2024-09-30 14:11:01 +0900

    refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,48 @@
-import TreeSelect from "@/components/ui/tree-select";
+import { type TreeNode, TreeSelect } from "@/components/ui/tree-select";
+
+const treeData: TreeNode[] = [
+	{
+		id: "0-0",
+		label: "0-0",
+		children: [
+			{
+				id: "0-0-0",
+				label: "0-0-0",
+				children: [
+					{ id: "0-0-0-0", label: "0-0-0-0" },
+					{ id: "0-0-0-1", label: "0-0-0-1" },
+					{ id: "0-0-0-2", label: "0-0-0-2" },
+				],
+			},
+			{
+				id: "0-0-1",
+				label: "0-0-1",
+				children: [
+					{ id: "0-0-1-0", label: "0-0-1-0" },
+					{ id: "0-0-1-1", label: "0-0-1-1" },
+					{ id: "0-0-1-2", label: "0-0-1-2" },
+				],
+			},
+			{ id: "0-0-2", label: "0-0-2" },
+		],
+	},
+	{
+		id: "0-1",
+		label: "0-1",
+		children: [
+			{ id: "0-1-0-0", label: "0-1-0-0" },
+			{ id: "0-1-0-1", label: "0-1-0-1" },
+			{ id: "0-1-0-2", label: "0-1-0-2" },
+		],
+	},
+	{ id: "0-2", label: "0-2" },
+];
 
 export default function Home() {
 	return (
 		<div className="flex flex-col items-center justify-center h-screen p-4 gap-4">
 			<h1>Hello World</h1>
-			<TreeSelect />
+			<TreeSelect data={treeData} />
 		</div>
 	);
 }
